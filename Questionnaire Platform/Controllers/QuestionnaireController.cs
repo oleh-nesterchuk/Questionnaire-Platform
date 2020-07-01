60063d63155ac41b00b762fc1bb10fb72f74b88f
Oleh Nesterchuk <olegnesterchuk@gmail.com>
2020-07-01 15:06:10 +0300

    Add Questionnaire controller. Register services at Startup

---
+++ Questionnaire Platform/Controllers/QuestionnaireController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Questionnaire.Core.Abstractions.Services;
+using Questionnaire.Core.Dto;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Questionnaire.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionnaireController : ControllerBase
+    {
+        private readonly IQuestionnaireService _questionnaireService;
+
+        public QuestionnaireController(IQuestionnaireService questionnaireService)
+        {
+            _questionnaireService = questionnaireService;
+        }
+
+        [HttpGet]
+        public ICollection<QuestionnaireInfoDto> Get()
+        {
+            return _questionnaireService.GetAllQuestionnaires();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<FullQuestionnaireDto> Get(int id)
+        {
+            return await _questionnaireService.GetFullQuestionnaireByIdAsync(id);
+        }
+
+        [HttpPost]
+        public async Task<QuestionnaireInfoDto> Post([FromBody] FullQuestionnaireDto questionnaireDto)
+        {
+            return await _questionnaireService.AddQuestionnaireAsync(questionnaireDto);
+        }
+
+        [HttpPut]
+        public async Task<QuestionnaireInfoDto> Put([FromBody] QuestionnaireInfoDto questionnaireDto)
+        {
+            return await _questionnaireService.UpdateQuestionnaireAsync(questionnaireDto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            await _questionnaireService.DeleteQuestionnaireAsync(id);
+        }
+    }
+}
--- Questionnaire Platform/Startup.cs
+++ Questionnaire Platform/Startup.cs
@@ -1,24 +1,40 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Questionnaire.Core.Abstractions;
+using Questionnaire.Core.Abstractions.Services;
+using Questionnaire.Core.Automapper;
+using Questionnaire.Dal;
+using Questionnaire.Services;
 
 namespace Questionnaire.Api
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<QuestionnaireDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("QuestionnaireDbConnection")));
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IQuestionnaireService, QuestionnaireService>();
+
+            services.AddAutoMapper(typeof(MappingProfile));
+
+            services.AddControllers();
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -30,10 +46,7 @@ namespace Questionnaire.Api
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
